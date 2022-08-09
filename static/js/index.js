b6040d5f5de7292b7db0f7d609de0a853ced0765
Marcus Martin <marclmart95@gmail.com>
2022-08-09 07:19:13 -0500

    dev

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,16 @@
 //jshint esversion: 6
 
+const body = document.querySelector('body');
+function disableScrolling(){
+  body.style.height = '100vh';
+  body.style.overflow = 'hidden';
+}
+
+function enableScrolling(){
+    body.style.height = 'unset';
+    body.style.overflowY = 'scroll';
+}
+
 //mobile menu code
 
 const mobileMenu = document.querySelector('.mobile-menu');
@@ -11,12 +22,14 @@ function menuOpen (){
   mobileMenu.style.height = "100vh";
   mobileMenu.style.width = "100vw";
   mobileMenu.style.opacity = "1";
+  disableScrolling();
 }
 
 function menuClose (){
   mobileMenu.style.height ="0";
   mobileMenu.style.width ="0";
   mobileMenu.style.opacity = "0";
+  enableScrolling();
 }
 
 hamburger.addEventListener('click', menuOpen);
@@ -27,11 +40,56 @@ menuItems.addEventListener('click', menuClose);
 
 const update = document.querySelector("#update");
 function hideUpdate(){
-
   update.style.display = 'none';
-  enableScrolling();
 }
 
 function showUpdate(){
-  update.style.display = "block"
+  update.style.display = "block";
 }
+
+//floating label on forms
+const FloatLabel = (() => {
+
+  // add active class
+  const handleFocus = (e) => {
+    const target = e.target;
+    target.parentNode.classList.add('active');
+    target.setAttribute('placeholder', target.getAttribute('data-placeholder'));
+  };
+
+  // remove active class
+  const handleBlur = (e) => {
+    const target = e.target;
+    if(!target.value) {
+      target.parentNode.classList.remove('active');
+    }
+    target.removeAttribute('placeholder');
+  };
+
+  // register events
+  const bindEvents = (element) => {
+    const floatField = element.querySelector('.input');
+    floatField.addEventListener('focus', handleFocus);
+    floatField.addEventListener('blur', handleBlur);
+  };
+
+  // get DOM elements
+  const init = () => {
+    const floatContainers = document.querySelectorAll('.form-input');
+
+    floatContainers.forEach((element) => {
+
+      if (element.querySelector('.input').value) {
+        element.classList.add('active');
+      }
+
+      bindEvents(element);
+    });
+  };
+
+  return {
+    init: init
+  };
+})();
+
+FloatLabel.init();
