54a0a24d7147f935f29df2147e87a3a0e616fa6e
Marcus Martin <marclmart95@gmail.com>
2024-05-31 07:05:07 -0500

    fixed forms

---
+++ isableFastRender/js/index.js
@@ -0,0 +1,111 @@
+//jshint esversion: 6
+
+const body = document.querySelector('body');
+function disableScrolling(){
+  body.style.height = '100vh';
+  body.style.overflow = 'hidden';
+}
+
+function enableScrolling(){
+    body.style.height = 'unset';
+    body.style.overflowY = 'scroll';
+}
+
+//mobile menu code
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const hamburger = document.querySelector('.hamburger');
+const menuCloseButton = document.querySelector('.menu-close');
+const menuItems = document.querySelector('.menu-item');
+
+function menuOpen (){
+  mobileMenu.style.height = "100vh";
+  mobileMenu.style.width = "100vw";
+  mobileMenu.style.opacity = "1";
+  disableScrolling();
+}
+
+function menuClose (){
+  mobileMenu.style.height ="0";
+  mobileMenu.style.width ="0";
+  mobileMenu.style.opacity = "0";
+  enableScrolling();
+}
+
+hamburger.addEventListener('click', menuOpen);
+
+menuCloseButton.addEventListener('click', menuClose);
+
+menuItems.addEventListener('click', menuClose);
+
+const update = document.querySelector("#update");
+function hideUpdate(){
+  update.style.display = 'none';
+}
+
+function showUpdate(){
+  update.style.display = "block";
+}
+
+function formChoice(){
+  let form = document.getElementById('choice').value
+  console.log(form)
+  let minnesota = document.getElementById('minnesota');
+  let texMex = document.getElementById("texmex")
+  if (form == "Minnesota"){
+     minnesota.classList.remove("no-display")
+     texMex.classList.add('no-display')
+     console.log(texMex, minnesota)
+  } else if(form == "Texas-Mexico-Border"){
+    console.log(minnesota, texMex)
+    texMex.classList.remove('no-display')
+    minnesota.classList.add('no-display')
+  }
+}
+
+//floating label on forms
+const FloatLabel = (() => {
+
+  // add active class
+  const handleFocus = (e) => {
+    const target = e.target;
+    target.parentNode.classList.add('active');
+    target.setAttribute('placeholder', target.getAttribute('data-placeholder'));
+  };
+
+  // remove active class
+  const handleBlur = (e) => {
+    const target = e.target;
+    if(!target.value) {
+      target.parentNode.classList.remove('active');
+    }
+    target.removeAttribute('placeholder');
+  };
+
+  // register events
+  const bindEvents = (element) => {
+    const floatField = element.querySelector('.input');
+    floatField.addEventListener('focus', handleFocus);
+    floatField.addEventListener('blur', handleBlur);
+  };
+
+  // get DOM elements
+  const init = () => {
+    const floatContainers = document.querySelectorAll('.form-input');
+
+    floatContainers.forEach((element) => {
+
+      if (element.querySelector('.input').value) {
+        element.classList.add('active');
+      }
+
+      bindEvents(element);
+    });
+  };
+
+  return {
+    init: init
+  };
+})();
+
+FloatLabel.init();
